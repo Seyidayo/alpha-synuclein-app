a7056ec45cd7e14cf66c6ad78357b20df94370c1
Idowu Faith <faithid10@gmail.com>
2019-03-31 17:19:07 +0100

    Chore(Deploy): Set up for Heroku deployment

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const app = express();
 const routes = require("./routes");
+const path = require("path");
 const PORT = process.env.PORT || 5000;
 
 require("./models");
 // configure body parser for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.use(express.static("client/build"));
-// routes
 app.use("/", routes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+// routes
 
 // Bootstrap server
 app.listen(PORT, () => {
